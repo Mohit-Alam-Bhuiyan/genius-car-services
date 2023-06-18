2821ce6e429c97bd0a6558efceaaab8c0b0e1ff9
Mohit Alam Bhuiyan <mohitcseedu@gmail.com>
2023-06-18 12:53:56 +0600

    app part updated

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap';
 import logo from '../../../images/logo.png'
 import { Link, NavLink } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { signOut } from 'firebase/auth';
 const Header = () => {
+    const [user] = useAuthState(auth);
+
+    const handleSignOut = () => {
+      signOut(auth);
+    }
+
     return (
     <>   
     <Navbar collapseOnSelect expand="lg" sticky='top' bg="primary" variant="dark">
@@ -29,9 +38,13 @@ const Header = () => {
           </Nav>
           <Nav>
             <Nav.Link as={Link} to="about">About</Nav.Link>
-            <Nav.Link as={Link} to="login">
+            {
+               user ?
+                  <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>sign out</button>
+               :
+              <Nav.Link as={Link} to="login">
              Login
-            </Nav.Link>
+            </Nav.Link>}
           </Nav>
         </Navbar.Collapse>
       </Container>
